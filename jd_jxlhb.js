d0e9dd051f5557807c75bfe59a8c6147fb8d5c5e
sltalex <51757517@qq.com>
2022-01-27 10:25:59 +0800

    update

--- jd_jxlhb.js
+++ jd_jxlhb.js
@@ -44,7 +44,7 @@ const BASE_URL = 'https://m.jingxi.com/cubeactive/steprewardv3'
       '活动入口：京喜app-》我的-》京喜领88元红包\n' +
       '助力逻辑：先自己京东账号相互助力，如有剩余助力机会，则助力作者\n' +
       '温馨提示：如提示助力火爆，可尝试寻找京东客服')
-  let res = await getAuthorShareCode('')
+  let res = await getAuthorShareCode('vjyP1CfZK-Muzq-zCQVMaPHVJ4QrPhOOKHvY2tzwz10') //m
   if (!res) {
     $.http.get({url: ''}).then((resp) => {}).catch((e) => $.log('刷新CDN异常', e));
     await $.wait(1000)
